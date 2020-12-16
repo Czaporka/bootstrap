e722bbb51183c3dd6fdf03851e20ed916b133986
Czaporka <30650003+Czaporka@users.noreply.github.com>
2020-12-16 23:20:52 +0100

    Update colors in status report

--- _scripts/print-status.sh
+++ _scripts/print-status.sh
@@ -4,21 +4,21 @@ set -u
 A="${1/.sh/}"  # installed, i.e. present in ~/...
 B="${2}"       # rendered, i.e. present in ./target/...
 
-RED="\033[31m"
-RED_BOLD="\033[31;1m"
-GREEN="\033[32m"
-YELLOW="\033[33m"
+C_NOT_INSTALLED="\033[90m"
+C_ERROR="\033[91;1m"
+C_UP_TO_DATE="\033[32m"
+C_DIFFERS="\033[31m"
 RESET="\033[0m"
 
 if [[ ! -f ${A} ]]; then
-    echo -e "${RED}[Not installed]: ${A}${RESET}"
+    echo -e "${C_NOT_INSTALLED}[Not installed]: ${A}${RESET}"
     exit 0
 fi
 diff -q ${A} ${B} > /dev/null
 case $? in
-0) echo -e "${GREEN}[Up to date   ]: ${A}${RESET}" ;;
-1) echo -e "${YELLOW}[Differs      ]: ${A}${RESET}" ;;
-2) echo -e "${RED_BOLD}[    ERROR    ]: ${A}${RESET}" ;;
+0) echo -e "${C_UP_TO_DATE}[Up to date   ]: ${A}${RESET}" ;;
+1) echo -e "${C_DIFFERS}[Differs      ]: ${A}${RESET}" ;;
+2) echo -e "${C_ERROR}[    ERROR    ]: ${A}${RESET}" ;;
 esac
 
 exit 0
